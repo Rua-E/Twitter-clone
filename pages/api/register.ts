54e684e0190d711a9ab9af8dc970b74b6e1c0096
Rua <rua.elkasheef@gmail.com>
2024-05-17 21:48:25 -0400

    working on register file

---
+++ pages/api/register.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest, 
+    res: NextApiResponse
+    ) {
+        if (req.method !=== 'POST') {
+            return res.status(405).end();
+        }
+        try {
+
+        } catch (error) {
+            console.log(error)
+            return res.
+        }
+    }
+  
