ba4e91e8f6b99ee8730e599a4cbf7af2c4dd201d
Rua <rua.elkasheef@gmail.com>
2024-05-19 21:52:05 -0400

    can not create an account

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,10 +1,14 @@
 import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
 
 import Input from "../Input";
 import Modal from "../Modal";
+
+import axios from "axios";
 
 const RegisterModal = () => {
     const loginModal = useLoginModal();
@@ -29,16 +33,29 @@ const RegisterModal = () => {
             try {
                 setIsLoading(true);
 
-                //TODO ADD REGISTER AND LOGIN
+                await axios.post('/api/register', {
+                    email,
+                    password,
+                    username,
+                    name
+                });
+
+                toast.success('Account created.');
+
+                signIn('credentials', {
+                    email,
+                    password
+                });
 
                 registerModal.onClose();
 
             } catch (error) {
                 console.log(error);
+                toast.error('Something went wrong.');
             } finally {
                 setIsLoading(false);
             }
-    }, [registerModal]);
+    }, [registerModal, email, password, username, name]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,20 @@ import Layout from "../components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
 import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 // import Modal from "@/components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return ( 
-    <>
+    <SessionProvider session={pageProps.session} >
       {/* <Modal actionLabel="Submit" isOpen title="Test Modal" />  */}
+      <Toaster />
       <RegisterModal />
       <LoginModal />
       <Layout >
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   )
 }
