285297d09115719d638868e568c8ce60778d39db
Rua <rua.elkasheef@gmail.com>
2024-05-19 22:47:09 -0400

    login modal completed

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -2,29 +2,38 @@ import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { IconType } from "react-icons";
 
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useLoginModal from "@/hooks/useLoginModal";
+
 interface SidebarItemProps {
     label: string;
     href?: string;
     icon: IconType
     onClick?: () => void;
+    auth?: boolean;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
     label,
     href,
     icon: Icon,
-    onClick
+    onClick,
+    auth
 }) => {
+    const loginModal = useLoginModal();
+    const { data: currentUser } = useCurrentUser();
     const router = useRouter();
     const handleClick = useCallback(() => {
         if (onClick) {
             return onClick();
         }
 
-        if (href) {
+        if(auth && !currentUser) {
+            loginModal.onOpen();
+        } else if (href) {
             router.push(href);
         }
-    }, [router, onClick, href]);
+    }, [router, onClick, href, currentUser, auth, loginModal]);
 
     return (
         <div onClick={handleClick} className="flex flex-row items-center">
--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
@@ -27,7 +28,10 @@ const LoginModal = () => {
             try {
                 setIsLoading(true);
 
-                //TODO ADD LOG IN
+                await signIn('credentials', {
+                    email,
+                    password
+                })
 
                 loginModal.onClose();
 
@@ -36,7 +40,7 @@ const LoginModal = () => {
             } finally {
                 setIsLoading(false);
             }
-    }, [loginModal]);
+    }, [loginModal, email, password]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -48,6 +52,7 @@ const LoginModal = () => {
             />
             <Input 
                 placeholder="Password"
+                type="password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
                 disabled={isLoading}
