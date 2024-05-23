168288e01922e28616266b8563d50a575638f6b3
Rua <rua.elkasheef@gmail.com>
2024-05-22 23:03:26 -0400

    created editModal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
 import Layout from "../components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
+import EditModal from "@/components/modals/EditModal";
 import "@/styles/globals.css";
-import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
 // import Modal from "@/components/Modal";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <SessionProvider session={pageProps.session} >
       {/* <Modal actionLabel="Submit" isOpen title="Test Modal" />  */}
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout >
