1f52bcf627406439be8350de53118b21770fc3da
Rua <rua.elkasheef@gmail.com>
2024-06-30 20:24:41 -0400

    working on useFollows

---
+++ hooks/useFollow.ts
@@ -0,0 +1,26 @@
+import { useCallback, useMemo } from "react";
+
+import useCurrentUser from "./useCurrentUser";
+import useLoginModal from "./useLoginModal";
+import useUser from "./useUser";
+
+const useFollow =  (userId: string) => {
+   const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser(); 
+   const { mutate: mutateFetchedUser } = useUser(userId);
+
+   const loginModal = useLoginModal();
+
+   const isFollowing = useMemo(() => {
+    const list = currentUser?.followingIds || [];
+
+    return list.includes(userId);
+   }, [userId, currentUser?.followingIds]);
+
+   const toggleFollow = useCallback(() => {
+    if (!currentUser) {
+        return loginModal.onOpen();
+    }
+
+   }, []);
+
+}
