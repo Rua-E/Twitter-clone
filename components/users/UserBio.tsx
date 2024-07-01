01f49b93360553c30c869e47d99389fad9c055fd
Rua <rua.elkasheef@gmail.com>
2024-06-30 20:47:24 -0400

    completed follow button

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -4,7 +4,7 @@ import { BiCalendar } from 'react-icons/bi';
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
-
+import useFollow from '@/hooks/useFollow';
 import useEditModal from '@/hooks/useEditModal';
 
 import Button from '../Button';
@@ -18,6 +18,8 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     const { data: fetchedUser } = useUser(userId);
 
     const editModal = useEditModal();
+
+    const { isFollowing, toggleFollow } = useFollow(userId);
 
     const createdAt = useMemo(() => {
         if (!fetchedUser?.createdAt) {
@@ -34,9 +36,10 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
                     <Button secondary label="Edit" onClick={editModal.onOpen} />
                 ) : (
                     <Button 
-                        onClick={() => {}}
-                        label="Follow"
-                        secondary
+                        onClick={toggleFollow}
+                        label={isFollowing ? 'Unfollow' : 'Follow'}
+                        secondary={!isFollowing}
+                        outline={isFollowing}
                     />
                 )}
             </div>
